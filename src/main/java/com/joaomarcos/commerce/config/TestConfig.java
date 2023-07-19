593ec92aa7cf605bebeb17fae3ab3f580c0eb60e
joaomcsferreira <marcosc024@gmail.com>
2023-07-18 21:47:22 -0300

    feat: OrderStatus enum

--- src/main/java/com/joaomarcos/commerce/config/TestConfig.java
+++ src/main/java/com/joaomarcos/commerce/config/TestConfig.java
@@ -1,6 +1,7 @@
 package com.joaomarcos.commerce.config;
 
 import com.joaomarcos.commerce.entities.Order;
+import com.joaomarcos.commerce.entities.enums.OrderStatus;
 import com.joaomarcos.commerce.repositories.OrderRepository;
 import com.joaomarcos.commerce.repositories.UserRepository;
 import com.joaomarcos.commerce.entities.User;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User firstUser = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User secondUser = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order firstOrder = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), firstUser);
-        Order secondOrder = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), secondUser);
-        Order thirdOrder = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), firstUser);
+        Order firstOrder = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID,firstUser);
+        Order secondOrder = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,secondUser);
+        Order thirdOrder = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,firstUser);
 
         userRepository.saveAll(Arrays.asList(firstUser, secondUser));
         orderRepository.saveAll(Arrays.asList(firstOrder, secondOrder, thirdOrder));

bc5036de1300c6b89b9be12f820d15d256ee1959
joaomcsferreira <marcosc024@gmail.com>
2023-07-19 18:52:05 -0300

    feat: Exception handling - findById

--- src/main/java/com/joaomarcos/commerce/services/UserService.java
+++ src/main/java/com/joaomarcos/commerce/services/UserService.java
@@ -2,6 +2,7 @@ package com.joaomarcos.commerce.services;
 
 import com.joaomarcos.commerce.repositories.UserRepository;
 import com.joaomarcos.commerce.entities.User;
+import com.joaomarcos.commerce.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
     public User findById(Long id) {
         Optional<User> user = repository.findById(id);
 
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
---
+++ src/main/java/com/joaomarcos/commerce/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.joaomarcos.commerce.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}

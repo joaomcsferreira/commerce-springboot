781a8e70f944ab8cae862bf8ce6e5e56858603db
joaomcsferreira <marcosc024@gmail.com>
2023-07-19 13:10:43 -0300

    feat: Many-to-many association with JoinTable

--- src/main/java/com/joaomarcos/commerce/config/TestConfig.java
+++ src/main/java/com/joaomarcos/commerce/config/TestConfig.java
@@ -40,11 +40,22 @@ public class TestConfig implements CommandLineRunner {
         Category thirdCategory = new Category(null, "Computers");
 
         Product firstProduct = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
-        Product secondProdut = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product secondProduct = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
         Product thirdProduct = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
         Product fourthProduct = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
         Product fifthProduct = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
+        categoryRespository.saveAll(Arrays.asList(firstCategory, secondCategory, thirdCategory));
+        productRepository.saveAll(Arrays.asList(firstProduct, secondProduct, thirdProduct, fourthProduct, fifthProduct));
+
+        firstProduct.getCategories().add(firstCategory);
+        secondProduct.getCategories().add(firstCategory);
+        secondProduct.getCategories().add(thirdCategory);
+        thirdProduct.getCategories().add(thirdCategory);
+        fourthProduct.getCategories().add(thirdCategory);
+        fifthProduct.getCategories().add(secondCategory);
+
+        productRepository.saveAll(Arrays.asList(firstProduct, secondProduct, thirdProduct, fourthProduct, fifthProduct));
 
         User firstUser = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User secondUser = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
@@ -53,8 +64,6 @@ public class TestConfig implements CommandLineRunner {
         Order secondOrder = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,secondUser);
         Order thirdOrder = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,firstUser);
 
-        categoryRespository.saveAll(Arrays.asList(firstCategory, secondCategory, thirdCategory));
-        productRepository.saveAll(Arrays.asList(firstProduct, secondProdut, thirdProduct, fourthProduct, fifthProduct));
         userRepository.saveAll(Arrays.asList(firstUser, secondUser));
         orderRepository.saveAll(Arrays.asList(firstOrder, secondOrder, thirdOrder));
     }
--- src/main/java/com/joaomarcos/commerce/entities/Product.java
+++ src/main/java/com/joaomarcos/commerce/entities/Product.java
@@ -20,7 +20,8 @@ public class Product implements Serializable {
     private String imgUrl;
 
     @ManyToMany
-    private final Set<Category> categories = new HashSet<>();
+    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
 
     public Product() {}
 
